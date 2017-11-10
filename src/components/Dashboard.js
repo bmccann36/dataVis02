c668103fd0be5640d631359d0f745be14b08c2d0
Brian McCann <bmccann36@gmail.com>
2017-11-09 19:19:05 -0500

    tested percentage approach added lat percent display to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -12,14 +12,17 @@ export default class Dashboard extends Component {
 
 
   render(){
-    let lat = this.props.corr.lat
-
+    let val = this.props.coor.lat
+    let dist = val - 40.502 // subtract the value from the base (lowest value)
+    let ratio = dist/.4070 // divide that by the range (difference between high lat and low lat)
+    let percentage = ratio*100 // turn ito a percentage value
     console.log(this.props)
     return(
     <div className= "dashboard" >
     <h1> hello world </h1>
     <p> {this.props.coor.lng} longitude </p>
     <p> {this.props.coor.lat} latitude </p>
+    <p> {percentage}  percentage </p>
 
 
 
