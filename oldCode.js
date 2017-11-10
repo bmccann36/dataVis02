4e016cf44a22a6107704f9d9be222c7b37911603
Brian McCann <bmccann36@gmail.com>
2017-11-10 13:05:09 -0500

    able to assign addresses to data points needs testing

--- oldCode.js
+++ oldCode.js
@@ -18,3 +18,26 @@ for (let i = 0; i <= 50; i++) {
   testArr.push(obj)
 }
 
+
+ // UTILITY TO FIND HIGHEST AND LOWEST LAT LON
+const highestLat = grid.reduce( (acc, val) => {
+  return acc.lat > val.lat ? acc : val
+})
+// console.log('highest lat', highestLat.lat)
+// highest Lat  40.91
+
+const lowestLat = grid.reduce( (acc, val) => {
+  return acc.lat < val.lat ? acc : val
+})
+// console.log('lowest lat', lowestLat.lat)  // 40.502
+
+
+const highestLon = grid.reduce( (acc, val) => {
+  return acc.lon > val.lon ? acc : val
+})
+console.log(highestLon.lon, 'highest lon')  //lon: -73.706596761
+
+const lowestLon = grid.reduce( (acc, val) => {
+  return acc.lon < val.lon ? acc : val
+})
+console.log(lowestLon.lon, 'lowest lon')  //   lon: -74.249303727
