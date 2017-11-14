937b1ad31ec02a93d5648dc9707ff7d4e3491ecf
Brian McCann <Brian@Users-MacBook-Pro.local>
2017-11-14 09:42:26 -0500

    tweaks for demo

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 // const experiments = require('./code/experiments')
 // const prepBigData = require('./backend/code/prepBigData.js')
 const chalk = require('chalk')
-
 // const data = require('./backend/data/focusedData')
 const grid = require('./src/grid')
 
 const bigData = require('./backend/data/crimeData').data
 
-console.log(bigData.length)
+magenta(bigData.length)
 //
 // lowest lat 40.4988   range 0.4139
-
 // lowest lon -74.25319084   range 0.552474155
 
-// let filtered = bigData.filter(entry => {
-//   return entry[29] > 40.609
-// })
+// RETURN ALL VALUES BETWEEN AN ARBITRARY RANGE ****
+let filtered = bigData.filter(entry => {
+  return (
+    (entry[29] > 40.609 && entry[29] < 40.620 ) &&
+    (entry[30] > -74.100 && entry[30] < -73.110))
+})
 
+cyan(filtered.length)
 // let sorted = bigData.sort(compareFunc)
 
 // function compareFunc(a, b) {
@@ -26,3 +28,12 @@ console.log(bigData.length)
 // }
 
 // console.log(chalk.magenta('sorted'))
+
+
+function magenta(str){
+  console.log(chalk.magenta(str))
+}
+
+function cyan(str) {
+  console.log(chalk.cyan(str))
+}
